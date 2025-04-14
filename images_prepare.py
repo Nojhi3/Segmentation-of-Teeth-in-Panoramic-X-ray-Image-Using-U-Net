2dcca1894fa306b80a9e6a60dc543c7779ce4157
Akshat <116896690+Nojhi3@users.noreply.github.com>
2025-04-14 19:24:52 +0530

    Update images_prepare.py

--- images_prepare.py
+++ images_prepare.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-
 @author: serdarhelli
 """
 
@@ -12,53 +11,38 @@ from natsort import natsorted
 
 
 def convert_one_channel(img):
-    #some images have 3 channels , although they are grayscale image
-    if len(img.shape)>2:
-        img=img[:,:,0]
-        return img
+    # Some images have 3 channels, although they are grayscale
+    if len(img.shape) > 2:
+        return img[:, :, 0]
     else:
         return img
-    
-def pre_images(resize_shape,path,include_zip):
-    if include_zip==True:
-        ZipFile(path+"/DentalPanoramicXrays.zip").extractall(path) 
-        path=path+'/Images/'
-    dirs=natsorted(os.listdir(path))
-    sizes=np.zeros([len(dirs),2])
-    images=img=Image.open(path+dirs[0])
-    sizes[0,:]=images.size
-    images=(images.resize((resize_shape),Image.ANTIALIAS))
-    images=convert_one_channel(np.asarray(images))
-    for i in range (1,len(dirs)):
-        img=Image.open(path+dirs[i])
-        sizes[i,:]=img.size
-        img=img.resize((resize_shape),Image.ANTIALIAS)
-        img=convert_one_channel(np.asarray(img))
-        images=np.concatenate((images,img))
-    images=np.reshape(images,(len(dirs),resize_shape[0],resize_shape[1],1))
-    return images,sizes
 
+def pre_images(resize_shape, path, include_zip):
+    # Handle Pillow version compatibility
+    if hasattr(Image, 'Resampling'):
+        resample_mode = Image.Resampling.LANCZOS
+    else:
+        resample_mode = Image.ANTIALIAS
 
+    if include_zip:
+        ZipFile(os.path.join(path, "DentalPanoramicXrays.zip")).extractall(path)
+        path = os.path.join(path, 'Images')
 
+    dirs = natsorted(os.listdir(path))
+    sizes = np.zeros((len(dirs), 2))
 
+    # Load first image
+    img = Image.open(os.path.join(path, dirs[0]))
+    sizes[0, :] = img.size
+    img_resized = img.resize(resize_shape, resample=resample_mode)
+    images = convert_one_channel(np.asarray(img_resized))
 
+    for i in range(1, len(dirs)):
+        img = Image.open(os.path.join(path, dirs[i]))
+        sizes[i, :] = img.size
+        img_resized = img.resize(resize_shape, resample=resample_mode)
+        img_array = convert_one_channel(np.asarray(img_resized))
+        images = np.concatenate((images, img_array))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    images = np.reshape(images, (len(dirs), resize_shape[0], resize_shape[1], 1))
+    return images, sizes
